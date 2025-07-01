9720c6f3f2bf13e90b048ffc2051a59c2d1b178b
thuan <thuan123@gmail.com>
2025-07-01 22:02:25 +0700

    feat(config)

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,9 +1,77 @@
 import './Detail.css';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ApartmentIcon from '@mui/icons-material/Apartment';
 
 function Detail() {
     return (
-        <div>
-            <h1>Detail Page</h1>
+        <div className='app'>
+            <header className='header'>TRAN VAN THUAN</header>
+                <p>
+                    <span className='icon-text-container'><EmailIcon></EmailIcon>thuantranuit2004@gmail.com</span>
+                    <span className='icon-text-container'> <PhoneIcon></PhoneIcon> (+84) 984 091 038</span>
+                    <span className='icon-text-container'> <GitHubIcon></GitHubIcon> https://github.com/thuantv-uit </span>
+                </p>
+                <hr/>
+                <h1>SUMMARY</h1>
+                    <p>I am a third-year student at the University of Information Technology VNU-HCM, majoring in Computer
+                    Networks and Data Communications. I am actively seeking job opportunities as a DevOps Engineer. I am
+                    committed to continuous learning and applying knowledge to grow in this field.</p>
+                    <hr/>
+                <h1>EDUCATION</h1>
+                    <p><b>UNIVERSITY OF INFORMATION TECHNOLOGY - VNUHCM</b></p>
+                    <p>Studying Computer Networks and Data Communications</p>
+                    <hr/>
+                <h1>PROJECT</h1>
+                {/* Project 1 */}
+                    <h2>CI/CD pipeline for a microservices application</h2>
+                        <p> Role: Team Leader (Team of 1) </p>
+                        <p> Description: Developed a dynamic timeline web app for creating, customizing, and managing timelines </p>
+                        <p>
+                            <li>APIs: Developed APIs for timeline management, user authentication, and invitations.</li>
+                            <li>Collaboration: Created a feature to view personal and invited timelines.</li>
+                            <li>Integration: Linked React frontend with Node.js backend for smooth API handling and data processing.</li>
+                        </p>
+                        <p> Technology: NodeJS, MongoDB, React </p>
+                {/* Project 2 */}
+                    <h2>Timeline</h2>
+                        <p>Role: Team Leader (Team of 1)</p>
+                        <p>Description: Developed a dynamic timeline web app for creating, customizing, and managing timelines</p>
+                        <p>
+                            <li>APIs: Developed APIs for timeline management, user authentication, and invitations.</li>
+                            <li>Collaboration: Created a feature to view personal and invited timelines.</li>
+                            <li>Integration: Linked React frontend with Node.js backend for smooth API handling and data processing.</li>
+                        </p>
+                        <p>Technology: NodeJS, MongoDB, React</p>
+                {/* Project 3 */}
+                    <h2>Kubernetes Cluster Monitoring and Automation</h2>
+                        <p>Role: Member (Team of 4)</p>
+                        <p>Description: Built a high-availability Kubernetes cluster with monitoring and automation.</p>
+                        <p>
+                            <li>Cluster Setup: Deployed a 3-node Kubernetes cluster (1 master, 2 workers) for scalability.</li>
+                            <li>Management: Utilized Rancher for a web-based interface to streamline cluster administration.</li>
+                            <li>Monitoring & Alerts: Integrated Prometheus and Grafana for real-time CPU, memory, and pod metrics visualization,
+                            with Alertmanager configured for email notifications at 70% node CPU.</li>
+                            <li>Autoscaling: Implemented Horizontal Pod Autoscaler for pod scaling at 80% CPU usage, ensuring an efficient,
+                            automated solution with minimal downtime.</li>
+                        </p>
+                        <p>Technologies: Kubernetes, Prometheus, Grafana, Rancher</p>
+                    {/* Project 4 */}
+                        <h2>Resort Network Infrastructure Design</h2>
+                        <p>Role: Team Leader (Team of 3)</p>
+                        <p>Description: Designed a network infrastructure for a 4-star Phan Thiet resort, compliant with TCVN</p>
+                        <p>
+                            <li>Network Design: Designed a wired and wireless network with a scalable and reliable logical topology.</li>
+                            <li>Wi-Fi: Deployed a resort-wide wireless network with a user-friendly Captive Portal for secure guest and staff access.</li>
+                            <li>Security: Implemented a camera surveillance system and site-to-site VPN to the corporate data center.</li>
+                        </p>
+                        <p>Technology: Cisco Packet Tracer</p>
+                    <hr/>
+                <h1>REFERENCES</h1>
+                    <span className='icon-text-container'><ApartmentIcon></ApartmentIcon>University of Information Technology, VNU-HCM</span>
+                    <span className='icon-text-container'> <PhoneIcon></PhoneIcon> (+84) 866 056 466</span>
+                    <span className='icon-text-container'> <EmailIcon></EmailIcon> tuanla@uit.edu.vn </span>
         </div>
     );
 }
--- src/components/Tool/Tool.js
+++ src/components/Tool/Tool.js
@@ -1,9 +1,73 @@
 import './Tool.css';
+import JavaScript from './images/Backend/JavaScript-Logo.png';
+import NodeJS from './images/Backend/nodejs-express.jpg';
+import npm from './images/Backend/npm.png';
+import yarn from './images/Backend/yarn.jpg';
+import css from './images/Frontend/css.png';
+import react from './images/Frontend/react.png';
+import docker from './images/Conterinerization/docker.png';
+import kubernetes from './images/Conterinerization/kubernetes.png';
+import rancher from './images/Conterinerization/rancher.jpg';
+import grafana from './images/Monitoring/grafana.jpg';
+import prometheus from './images/Monitoring/prometheus.png';
+import aws from './images/Cloud/aws.png';
+import trivy from './images/Others/trivy.png';
+import sonarqube from './images/Others/sonarqube.png';
+import github from './images/CI-CD/github.png';
+import gitlab from './images/CI-CD/gitlab.jpg';
 
 function Tool() {
     return (
         <div>
-            <h1>Tool Page</h1>
+            <header className='header'>Tools</header>
+                <h1>Back End</h1>
+                    <div class="image-container">
+                        <img src={JavaScript} alt="JavaScript logo" className="image-item" />
+                        <img src={NodeJS} alt="NodeJS logo" className="image-item" />
+                        <img src={npm} alt="npm logo" className="image-item" />
+                        <img src={yarn} alt="yarn logo" className="image-item" />
+                    </div>
+
+
+                <h1>Front End</h1>
+                    <div class="image-container">
+                            <img src={JavaScript} alt="JavaScript logo" className="image-item" />
+                            <img src={css} alt="css logo" className="image-item" />
+                            <img src={react} alt="react logo" className="image-item" />
+                            <img src={npm} alt="npm logo" className="image-item" />
+                            <img src={yarn} alt="yarn logo" className="image-item" />
+                    </div>
+
+                <h1>CI/CD</h1>
+                    <div class="image-container">
+                            <img src={github} alt="github logo" className="image-item" />
+                            <img src={gitlab} alt="gitlab logo" className="image-item" />
+                    </div>
+
+                <h1>Containerization</h1>
+                    <div class="image-container">
+                            <img src={docker} alt="docker logo" className="image-item" />
+                            <img src={kubernetes} alt="kubernetes logo" className="image-item" />
+                            <img src={rancher} alt="rancher logo" className="image-item" />
+                    </div>
+
+                <h1>Monitoring</h1>
+                    <div class="image-container">
+                            <img src={grafana} alt="grafana logo" className="image-item" />
+                            <img src={prometheus} alt="prometheus logo" className="image-item" />
+                    </div>
+
+                <h1>Cloud</h1>
+                    <div class="image-container">
+                            <img src={aws} alt="aws logo" className="image-item" />
+                    </div>
+
+                <h1>Other</h1>
+                    <div class="image-container">
+                            <img src={trivy} alt="trivy logo" className="image-item" />
+                            <img src={sonarqube} alt="sonarqube logo" className="image-item" />
+                    </div>
+            <></>
         </div>
     );
 }
